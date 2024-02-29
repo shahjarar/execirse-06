4c82a4304dbfdf5f4626acc1c3b1b559e3e32f0b
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 13:57:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var whitespaceName = "\n\tHamza Mirza\t\n";
+console.log(whitespaceName);
+var withoutWhitespaceName = 'Hamza Mirza'.trim();
+console.log(withoutWhitespaceName);
---
+++ main.ts
@@ -0,0 +1,6 @@
+let whitespaceName = "\n\tHamza Mirza\t\n";
+console.log(whitespaceName);
+
+let withoutWhitespaceName = 'Hamza Mirza'.trim();
+
+console.log(withoutWhitespaceName)
